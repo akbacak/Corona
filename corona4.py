5a23bbcc78303d131b127f444039746d89c0b5e8
akbacak <33653031+akbacak@users.noreply.github.com>
2020-07-03 15:23:33 +0300

    Add files via upload

---
+++ corona4.py
@@ -0,0 +1,110 @@
+import numpy as np
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report, confusion_matrix
+
+ 
+
+#Start
+train_data_path = 'coronaImages/train'
+val_data_path  = 'coronaImages/val'
+test_data_path  = 'coronaImages/test'
+img_rows = 150
+img_cols = 150
+epochs = 3
+batch_size = 32
+num_of_train_samples = 427
+num_of_val_samples  = 80
+num_of_test_samples  = 118
+ 
+
+#Image Generator
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+ 
+
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+val_datagen = ImageDataGenerator(rescale=1. / 255) 
+
+train_generator = train_datagen.flow_from_directory(train_data_path,
+                                                    target_size=(img_rows, img_cols),
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical')
+
+ 
+
+validation_generator = val_datagen.flow_from_directory(val_data_path,
+                                                        target_size=(img_rows, img_cols),
+                                                        batch_size=batch_size,
+                                                        class_mode='categorical')
+
+ 
+
+test_generator = val_datagen.flow_from_directory(test_data_path,
+                                                        target_size=(img_rows, img_cols),
+                                                        batch_size=batch_size,
+                                                        class_mode='categorical')
+
+
+
+
+
+
+# Build model
+model = Sequential()
+model.add(Convolution2D(32, (3, 3), input_shape=(img_rows, img_cols, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(32, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(64, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4))
+model.add(Activation('softmax'))
+ 
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+ 
+
+#Train
+model.fit_generator(train_generator,
+                    steps_per_epoch=num_of_train_samples // batch_size,
+                    epochs=epochs,
+                    validation_data=validation_generator,
+                    validation_steps=num_of_val_samples // batch_size)
+
+ 
+
+#Confution Matrix and Classification Report
+Y_pred = model.predict_generator(test_generator, num_of_test_samples // batch_size+1)
+y_pred = np.argmax(Y_pred, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(test_generator.classes, y_pred))
+print('Classification Report')
+target_names = ['1', '2', '3', '4']
+print(classification_report(test_generator.classes, y_pred, target_names=target_names))
+
+
+
+
+
