def091abd2f0298c2f8c81b2f4e37f393996a2cd
akbacak <33653031+akbacak@users.noreply.github.com>
2020-08-21 14:08:17 +0300

    Add files via upload

--- corona4.py
+++ corona4.py
@@ -12,16 +12,16 @@ from keras.applications import VGG16
 
 
 #Start
-train_data_path      = '../Datasets/train'
-val_data_path        = '../Datasets/val'
-test_data_path       = '../Datasets/test'
+train_data_path      = '../Datasets_2/train'
+val_data_path        = '../Datasets_2/val'
+test_data_path       = '../Datasets_2/test'
 img_rows             = 256
 img_cols             = 256
-epochs               = 100
+epochs               = 50
 batch_size           = 8
-num_of_train_samples = 675
-num_of_val_samples   = 231
-num_of_test_samples  = 142
+num_of_train_samples = 378 
+num_of_val_samples   = 98
+num_of_test_samples  = 115
 image_size           = 256 
 
 #Image Generator
@@ -61,18 +61,17 @@ test_generator = test_datagen.flow_from_directory(test_data_path,
 
 
 vgg_conv = VGG16(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
-for layer in vgg_conv.layers[:-4]:
-    layer.trainable = False
+for layer in vgg_conv.layers[:]:
+    layer.trainable = True
  
 
 model = Model(inputs = vgg_conv.input, outputs=vgg_conv.output) 
 Flatten = Flatten()(model.output)
 Dense_2 = Dense(4096)(Flatten)
 Dense_96 = Dense(512 ,activation='sigmoid')(Dense_2)
-Dense_5 = Dense(2, activation='softmax')(Dense_96)
+Dense_5 = Dense(4, activation='softmax')(Dense_96)
 model = Model(input=model.input, output=Dense_5)
-
-
+print(model.summary())
 
 
 
@@ -85,7 +84,7 @@ model.compile(loss='categorical_crossentropy',
 #Train
 model.fit_generator(train_generator,
                     #steps_per_epoch=num_of_train_samples // batch_size,
-                    steps_per_epoch=300 // batch_size,
+                    steps_per_epoch=num_of_train_samples // batch_size,
                     epochs=epochs,
                     validation_data=validation_generator,
                     validation_steps=num_of_val_samples // batch_size)
@@ -100,3 +99,4 @@ print(confusion_matrix(test_generator.classes, y_pred))
 print('Classification Report')
 target_names = [ 'covid', 'viral']
 print(classification_report(test_generator.classes, y_pred, target_names=target_names))
+'''
